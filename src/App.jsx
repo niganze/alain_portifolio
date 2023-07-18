2c1c1f9b62b23fb98e4dee614405bc9eedec0c58
Niganze alain <niganzealaian@gmail.com>
2023-07-18 22:16:45 +0200

    servives

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import Header from "./pages/Header";
 import Home from "./pages/Home";
+import Services from "./pages/Services";
 
 function App() {
   return (
     <div>
-      {/* <h1>Vite + React</h1>
-      <p className="read-the-docs">this is my app</p> */}
+      
       <Header />
       <Home />
+      <Services />
     </div>
   );
 }
